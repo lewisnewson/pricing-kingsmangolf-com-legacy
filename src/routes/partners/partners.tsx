a70c532b76c0c4e69916976a1b4dde6bb3fe9e1b
Lewis Newson <lewisnewson@gmail.com>
2024-05-09 23:01:18 +0100

    Started on the partners

--- src/routes/partners/partners.tsx
+++ src/routes/partners/partners.tsx
@@ -1,9 +1,35 @@
 import { getFirestore, collection, getDocs } from "firebase/firestore"
-import { Button, Space, Table } from "antd"
+import { Button, Space, Table, Modal, Input } from "antd"
 import { useEffect, useState } from "react"
 
 export default function Partners() {
 	const [allPartners, setAllPartners] = useState<any>([])
+	const [isModalOpen, setIsModalOpen] = useState(false)
+
+	const [addTo, setAddTo] = useState<any>({})
+	const [adding, setAdding] = useState(false)
+	const [name, setName] = useState("")
+	const [email, setEmail] = useState("")
+
+	const showModal = (record: any) => {
+		setAddTo(record)
+		setIsModalOpen(true)
+	}
+
+	const handleOk = async () => {
+		setAdding(true)
+
+		await new Promise((resolve) => setTimeout(resolve, 1000))
+
+		setIsModalOpen(false)
+	}
+
+	const handleCancel = () => {
+		setName("")
+		setEmail("")
+		setAddTo({})
+		setIsModalOpen(false)
+	}
 
 	useEffect(() => {
 		const fetchPartners = async () => {
@@ -28,30 +54,66 @@ export default function Partners() {
 	}, [])
 
 	return (
-		<Table
-			dataSource={allPartners}
-			columns={[
-				{
-					title: "Partner Name",
-					dataIndex: "name",
-					key: "name",
-					render: (text: any) => <a href={`/partners/test`}>{text}</a>,
-				},
-				{
-					title: "Open Requests",
-					dataIndex: "requests",
-					key: "requests",
-				},
-				{
-					title: "Action",
-					key: "action",
-					render: (_: any, record: any) => (
-						<Space size="middle">
-							<Button>Add User</Button>
-						</Space>
-					),
-				},
-			]}
-		/>
+		<>
+			<Table
+				dataSource={allPartners}
+				columns={[
+					{
+						title: "Partner Name",
+						dataIndex: "name",
+						key: "name",
+					},
+					{
+						title: "Open Requests",
+						dataIndex: "requests",
+						key: "requests",
+					},
+					{
+						title: "Actions",
+						key: "actions",
+						render: (_: any, record: any) => (
+							<Space size="middle">
+								<Button onClick={() => showModal(record)}>Add User</Button>
+							</Space>
+						),
+					},
+				]}
+			/>
+
+			<Modal
+				title={`Adding user to: ${addTo.name}`}
+				open={isModalOpen}
+				onOk={handleOk}
+				onCancel={handleCancel}
+				footer={[
+					<Button
+						key="back"
+						onClick={handleCancel}>
+						Cancel
+					</Button>,
+					<Button
+						key="submit"
+						type="primary"
+						loading={adding}
+						onClick={handleOk}>
+						Add User
+					</Button>,
+				]}>
+				<Input
+					type="text"
+					placeholder="Name"
+					value={name}
+					required
+					onChange={(e) => setName(e.target.value)}
+				/>
+				<Input
+					type="email"
+					placeholder="Email"
+					value={email}
+					required
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+			</Modal>
+		</>
 	)
 }
